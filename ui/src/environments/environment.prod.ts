8b9bbd6d294a0e9de886e22734f4898ab8e3b841
dkingjr <kingd9@gmail.com>
2020-07-05 17:46:03 -0400

    fixing build

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseUrl:
-    "https://enuwmyp6pl.execute-api.us-east-2.amazonaws.com/Prod/questions",
+  baseUrl:"https://enuwmyp6pl.execute-api.us-east-2.amazonaws.com/Prod/questions",
   production: true,
+  mockBackend: false
 };
